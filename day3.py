ee599a4f24c87bc08686f0bf3b48f2da5be83e14
Cashadi <ichascashadi@gmail.com>
2025-07-10 21:59:41 +0700

    learn pythin day 3

---
+++ day3.py
@@ -0,0 +1,17 @@
+#modify strings
+# a = " Hello, World!"
+# print(a.upper())
+# print(a.lower())
+# print(a.strip())
+# print(a.replace("H", "J"))
+
+#concatenate string
+# a = "Hello"
+# b = "World"
+# c = a + " " + b
+# print(c)
+
+#format string
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
