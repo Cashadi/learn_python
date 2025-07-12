34955b5f56b93e8ec90489d3b6b1cfbf3c10ed34
Cashadi <ichascashadi@gmail.com>
2025-07-12 12:38:37 +0700

    string methods

--- day4.py
+++ day4.py
@@ -1,3 +1,8 @@
 #python escape character
-txt = "We are the so-called \"Vikings\" from the north."
-print(txt)
+# txt = "We are the so-called \"Vikings\" from the north."
+# print(txt)
+
+#string methods
+txt = "hello, and welcome to my world."
+x = txt.capitalize()
+print (x)
