44d440beb4f9268bc51810cd68745076c560a0de
Cashadi <ichascashadi@gmail.com>
2025-07-22 13:36:38 +0700

    build triangle with python

--- day4.py
+++ day4.py
@@ -1,4 +1,8 @@
-text = "Belajar Python di DQLab."
-print(list(text))
-print(tuple(text))
-print(set(text))
+# text = "Belajar Python di DQLab."
+# print(list(text))
+# print(tuple(text))
+# print(set(text))
+
+#segitiga menggunakan python
+for i in range(1, 6):
+    print('*' * i)
