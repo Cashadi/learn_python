009d4be2271381d83b0d1ee036e2235b5c6b79b9
Cashadi <ichascashadi@gmail.com>
2025-07-23 13:35:54 +0700

    day 2

--- day4.py
+++ day4.py
@@ -1,8 +1,17 @@
+#day 1 learn python
+
 # text = "Belajar Python di DQLab."
 # print(list(text))
 # print(tuple(text))
 # print(set(text))
 
 #segitiga menggunakan python
-for i in range(1, 6):
-    print('*' * i)
+# for i in range(1, 6):
+#     print('*' * i)
+
+
+#day 2 learn python
+
+#comment
+print(10*2+5) #fungsi matematika
+print("Academy DQLab") #fungsi mencetak kalimat
