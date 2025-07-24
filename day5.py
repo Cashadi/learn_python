7e87c616ad274b812428a3a51f41d3f8b8d09e4f
Cashadi <ichascashadi@gmail.com>
2025-07-24 13:35:46 +0700

    praktek day2

--- day5.py
+++ day5.py
@@ -1,9 +1,15 @@
 #if elseif else
-i=5
+# i=5
+# if(i==5):
+#      print("ini adalah angka 5")
+# elif(i>5):
+#      print("lebih besar dari 5")
+# else:
+#      print("lebih kecil dari 5")
 
-if(i==5):
-     print("ini adalah angka 5")
-elif(i>5):
-     print("lebih besar dari 5")
+#mid praktek
+angka=5
+if(angka%2==0):
+    print("angka termasuk bilangan genap")
 else:
-     print("lebih kecil dari 5")
+    print("angka termasuk bilangan ganjil")
