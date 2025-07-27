da00a73049314a6208c69a6aeda254f907f142f4
Cashadi <ichascashadi@gmail.com>
2025-07-27 13:13:17 +0700

    boolean operation

--- day7.py
+++ day7.py
@@ -4,17 +4,24 @@
 # print(person['pekerjaan'])
 
 #praktek
-# Data yang dinyatakan ke dalam dictionary
-sepatu = {"nama": "Sepatu Niko", "harga": 150000, "diskon": 30000}
-baju = {"nama": "Baju Unikloh", "harga": 80000, "diskon": 8000}
-celana = {"nama": "Celana Lepis", "harga": 200000, "diskon": 60000}
-# Hitunglah harga masing-masing data setelah dikurangi diskon
-harga_sepatu = sepatu["harga"] - sepatu["diskon"]
-harga_baju = baju["harga"] - baju["diskon"]
-harga_celana = celana["harga"] - celana["diskon"]
-# Hitung harga total
-total_harga = harga_sepatu + harga_baju + harga_celana
-# Hitung harga kena pajak
-total_pajak = total_harga * 0.1
-# Cetak total_harga + total_pajak
-print(total_harga + total_pajak)
+# # Data yang dinyatakan ke dalam dictionary
+# sepatu = {"nama": "Sepatu Niko", "harga": 150000, "diskon": 30000}
+# baju = {"nama": "Baju Unikloh", "harga": 80000, "diskon": 8000}
+# celana = {"nama": "Celana Lepis", "harga": 200000, "diskon": 60000}
+# # Hitunglah harga masing-masing data setelah dikurangi diskon
+# harga_sepatu = sepatu["harga"] - sepatu["diskon"]
+# harga_baju = baju["harga"] - baju["diskon"]
+# harga_celana = celana["harga"] - celana["diskon"]
+# # Hitung harga total
+# total_harga = harga_sepatu + harga_baju + harga_celana
+# # Hitung harga kena pajak
+# total_pajak = total_harga * 0.1
+# # Cetak total_harga + total_pajak
+# print(total_harga + total_pajak)
+
+
+#operator
+bil1 = 5
+bil2 = bil1 // 2
+hasil = bil1 <= bil2 or bil1 == 2
+print(hasil)
