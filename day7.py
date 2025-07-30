403b0f653363ec5ec19340a389005a94f393be30
Cashadi <ichascashadi@gmail.com>
2025-07-30 13:18:41 +0700

    learn python day ..

--- day7.py
+++ day7.py
@@ -20,8 +20,25 @@
 # print(total_harga + total_pajak)
 
 
-#operator
-bil1 = 5
-bil2 = bil1 // 2
-hasil = bil1 <= bil2 or bil1 == 2
-print(hasil)
+# #operator
+# bil1 = 5
+# bil2 = bil1 // 2
+# hasil = bil1 <= bil2 or bil1 == 2
+# print(hasil)
+
+#tentang prioritas
+# bilangan = (5 % 3 ** 2) + (3 + 2 * 2) * (4 - 2)
+# print(bilangan)
+
+#latiha
+sepatu = { "nama" : "Sepatu Niko", "harga": 150000, "diskon": 30000 }
+baju = { "nama" : "Baju Unikloh", "harga": 80000, "diskon": 8000 }
+celana = { "nama" : "Celana Lepis", "harga": 200000, "diskon": 60000 }
+
+harga_sepatu = sepatu["harga"] - sepatu["diskon"]
+harga_baju = baju["harga"] - baju["diskon"]
+harga_celana = celana["harga"] - celana["diskon"]
+
+total_harga = (harga_sepatu + harga_baju + harga_celana) * 1.1
+
+print(total_harga)
