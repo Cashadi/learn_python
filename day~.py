c09ed18eb510f0472743aa18a015bfbc7a76eb00
Cashadi <ichascashadi@gmail.com>
2025-08-13 00:44:48 +0700

    learn nested loops

--- day~.py
+++ day~.py
@@ -14,17 +14,33 @@
 # print(total_tagihan)
 
 #python loops break
-tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
-i = 0
-jumlah_tagihan = len(tagihan)
-total_tagihan = 0
-while i < jumlah_tagihan:
-    # jika terdapat tagihan ke-i yang bernilai minus (di bawah nol),
-    # pengulangan akan dihentikan
-    if tagihan[i] < 0:
-        total_tagihan = -1
-        print("terdapat angka minus dalam tagihan, perhitungan dihentikan!")
-        break
-    total_tagihan += tagihan[i]
-    i += 1
-print(total_tagihan)
+# tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+# i = 0
+# jumlah_tagihan = len(tagihan)
+# total_tagihan = 0
+# while i < jumlah_tagihan:
+#     # jika terdapat tagihan ke-i yang bernilai minus (di bawah nol),
+#     # pengulangan akan dihentikan
+#     if tagihan[i] < 0:
+#         total_tagihan = -1
+#         print("terdapat angka minus dalam tagihan, perhitungan dihentikan!")
+#         break
+#     total_tagihan += tagihan[i]
+#     i += 1
+# print(total_tagihan)
+
+
+#for loops part 1
+# list_tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+# total_tagihan = 0
+# for tagihan in list_tagihan:
+#     total_tagihan += tagihan
+# print(total_tagihan)
+
+
+#nested loops
+list_daerah = ['Malang', 'Palembang', 'Medan']
+list_buah = ['Apel', 'Duku', 'Jeruk']
+for nama_daerah in list_daerah:
+    for nama_buah in list_buah:
+        print(nama_buah+" "+nama_daerah)
